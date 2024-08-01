71b2b18b3c98e6dc471de2c578de10afe3d3f095
rubythebelle <rubybellekim@gmail.com>
2024-08-01 14:38:00 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ruby Kim on 2024-08-01.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
